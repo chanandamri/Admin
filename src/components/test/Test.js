49caef518d2b66c3f959fc71441925e306960350
avraham schmidt <avibatst@gmail.com>
2022-06-26 17:47:28 +0300

    17.48

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,9 +1,16 @@
+import Table from "../Admin/Table";
 
-function Test(){
-    return <>
-    
-    </>
+function Test() {
+  let list = {
+    classroom_name: "class a",
+    classroom_ID: "#123.456",
+    student: "10",
+  };
+  return (
+    <div>
+      <Table list={list} />
+    </div>
+  );
 }
 
-
-export default Test
+export default Test;
