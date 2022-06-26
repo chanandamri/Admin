d7c50977f51138d536d9ce5b9c196f72b0e42b7a
ariel gold <ariel6185@gmail.com>
2022-06-26 23:50:32 +0300

    header style

--- src/components/Admin/Header/index.js
+++ src/components/Admin/Header/index.js
@@ -1,21 +1,22 @@
-import './style.css';
-import {Link, useParams} from 'react-router-dom'
+import "./style.css";
+import { Link, useParams } from "react-router-dom";
 
 // Creator : Team E - Chanan
-function Header(){
-    const title = useParams()
-    return( <>
-    <div className="admin-header">
-        <img id="logo1" src={require("./logo1.png")} alt="logo1" />
-        <img id="logo2" src={require("./logo2.png")} alt="logo2" />
-        <div className="header-title">
-            <Link to="/classrooms">Classrooms</Link>
-           {/* <div>{title}</div> */}
-        </div>
-        <img id="user-logo" src={require("./user-logo.png")} />
-    </div>
-    </>
-)}
+function Header() {
+    const title = useParams();
+    return (
+        <>
+            <div className="admin-header">
+                <img id="logo1" src={require("./logo1.png")} alt="logo1" />
+                <img id="logo2" src={require("./logo2.png")} alt="logo2" />
+                <Link className="header-title" to="/classrooms">
+                    Classrooms
+                </Link>
+                {/* <div>{title}</div> */}
+                <img id="user-logo" src={require("./user-logo.png")} />
+            </div>
+        </>
+    );
+}
 
-
-export default Header
+export default Header;
