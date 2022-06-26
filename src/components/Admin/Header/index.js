507c6f59fc855dbfe7688ef3ced79891bcd507c1
ariel gold <ariel6185@gmail.com>
2022-06-27 00:46:29 +0300

    header style

--- src/components/Admin/Header/index.js
+++ src/components/Admin/Header/index.js
@@ -1,19 +1,20 @@
+import * as React from "react"
 import "./style.css";
 import { Link, useParams } from "react-router-dom";
 
-// Creator : Team E - Chanan
+// Creator : Team E - Chanan: Chagay & Ariel
 function Header() {
-    const title = useParams();
+    // const {classId} = useParams();
     return (
         <>
             <div className="admin-header">
-                <img id="logo1" src={require("./logo1.png")} alt="logo1" />
-                <img id="logo2" src={require("./logo2.png")} alt="logo2" />
+                <img className="logo1" src={require("./logo1.png")} alt="logo1" />
+                <img className="logo2" src={require("./logo2.png")} alt="logo2" />
                 <Link className="header-title" to="/classrooms">
                     Classrooms
                 </Link>
-                {/* <div>{title}</div> */}
-                <img id="user-logo" src={require("./user-logo.png")} />
+                {/* <div className="header-title-route"></div> */}
+                <img className="user-logo" src={require("./user-logo.png")} />
             </div>
         </>
     );
