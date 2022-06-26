4146f13df4915731c37957f2874e978913fda591
Damri <damri@hp.com>
2022-06-26 17:54:56 +0300

    fix test

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,30 +1,9 @@
-<<<<<<< HEAD
-import Table from "../Admin/Table";
 
 function Test() {
-  let list = {
-    classroom_name: "class a",
-    classroom_ID: "#123.456",
-    student: "10",
-  };
   return (
     <div>
-      <Table list={list} />
     </div>
   );
-=======
-import '../Admin/nav'
-import Nav from '../Admin/nav'
-
-
-
-function Test() {
-    return (
-        <>
-<Nav></Nav>
-        </>
-    )
->>>>>>> faadf387fbd2808af52e463391d34365bf2eb2b3
 }
 
 export default Test;
