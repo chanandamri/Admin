e043a6ba9b3980370448333d8f6d3fd37b50ffc7
chagai <chagaiR@gmail.com>
2022-06-26 17:44:12 +0300

    first Header

---
+++ src/components/Admin/Header/index.js
@@ -0,0 +1,17 @@
+import styles from './style.module.css'
+
+// Creator : Team E - Chanan;
+function Header(){
+    return <>
+    <div className='admin-header'>
+        <div className='header-title'></div>
+        <div className='header-title-routh'></div>
+        <div className='bell'></div>
+        <div className='user-logo'></div>
+        
+    </div>
+    </>
+}
+
+
+export default Header;
