99186b8c0e0546c5255d4bf9276474f5357cce81
avraham schmidt <avibatst@gmail.com>
2022-06-27 10:33:21 +0300

    10:33

--- src/components/Admin/Table/index.jsx
+++ src/components/Admin/Table/index.jsx
@@ -2,19 +2,33 @@ import"./style.css";
 
 // Creator : Team E 
 
-function Table(props) {
-  let list = props.list;
-  for(let i=0;i<2;i++){
-  return (
-<div className="row_s">
-    <div className={"row"}>
-      <div className="text_row">{list[i].classroom_name}</div>
-      <div className="text_row">{list[i].classroom_ID}</div>
-      <div className="text_row">{list[i].student}</div>
-      <div className="text_row">edit  delt</div>
-     
-    </div></div>
-  );
-}}
+function Table() {
+  let list = [
+    {
+      classroom_name: "class a",
+      classroom_ID: "#123.456",
+      student: "10",
+      action:"dl"
+    },
+    {
+      classroom_name: "class b",
+      classroom_ID: "#123.789",
+      student: "15",
+    },
+  ];
+
+  return(
+    <>
+    <div className="row_s"></div>
+      <div className="row">
+    {Object.keys(list[0]).map((v)=>{
+    
+      return(<div className="text_row">{v}</div>)})}
+ </div>
+  
+  </>
+  )
+  
+}
 
 export default Table;
---
+++ src/components/test/Test.js
@@ -0,0 +1,13 @@
+import Table from "../Admin/Table";
+import Header from "../Admin/Header";
+
+function Test() {
+  return (
+    <div>
+      <Header />
+      <Table />
+    </div>
+  );
+}
+
+export default Test;
