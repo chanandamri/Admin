07bc3e0ce26a34b8445af38d634015ca9c48a324
ariel gold <ariel6185@gmail.com>
2022-06-28 12:43:56 +0300

    i

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,11 +1,6 @@
-import AdminLayout from "../Admin/AdminLayout/AdminLayout";
 
 export default function Test() {
   return (
     <div></div>
   )
-}
-  return <div>
-    <AdminLayout />
-  </div>;
 }
