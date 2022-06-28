536ea8cdc4d01192af1a057f28923a78ed2a5254
ariel gold <ariel6185@gmail.com>
2022-06-28 17:34:49 +0300

    i

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,9 +1,7 @@
-import AdminPermissionUi from "../../pages/Admin/AdminPermissionUi";
-import AdminLayout from "../Admin/AdminLayout";
 
 export default function Test() {
 
   return <div>
-    <AdminLayout />
+
   </div>;
 }
