efb649afcdee8b50e46bde804a90a4d3e9e5eb6b
ariel gold <ariel6185@gmail.com>
2022-06-29 10:24:37 +0300

    i

--- src/pages/Admin/StudentDetails/index.js
+++ src/pages/Admin/StudentDetails/index.js
@@ -1,13 +1,16 @@
 import { useEffect, useContext } from 'react';
 import { headerText } from '../../../context/hederText';
 import './style.css'
+// import assessment graph component
+// import TF graph component
+// import TR graph component
 
 // Creator : Team E - Ariel
 export default function StudentDetails(props){
     const {firstName, lastName, TR ,TF, assessment} = props;
 
     
-    const sortedList = Array(props).sort((a,b)=>a.name.common> b.name.common?1:-1)
+    const sortedList = Array(props).sort((a,b)=>a.date> b.date?1:-1)
     
     const [hederText, setHeaderText] = useContext(headerText);
 
@@ -22,16 +25,16 @@ export default function StudentDetails(props){
             <h1 className="name">{firstName} {lastName}</h1>
             <div className="graphs">
                 <div className="assessment">
-                    <h5>assessment {}</h5>
-                    {/* {assessment.graph} */}
+                    <h5>assessment {assessment}</h5>
+                    {/* <assessment graph component({assessment.graph})> */}
                 </div>
                 <div className="reading">
                     <h5>reading {TR}</h5>
-                    {/* {TR.graph} */}
+                    {/* <TR graph component({TR.graph})> */}
                 </div>
                 <div className="focus">
                     <h5>focus {TF}</h5>
-                    {/* {TF.graph} */}
+                    {/* <TF graph component({TF.graph})> */}
                 </div>
                 <hr />
                 <div>
