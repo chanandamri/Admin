ed469dfad9b8596403a33a1308645ef134107ca6
chanandamri <damrica@gmail.com>
2022-06-30 14:43:38 +0300

    changed input

--- src/pages/Admin/Login/index.jsx
+++ src/pages/Admin/Login/index.jsx
@@ -3,39 +3,40 @@ import { Link, useNavigate } from "react-router-dom";
 import MainButton from "../../../components/Admin/MainButton";
 import { useContext } from "react";
 import { userContext } from "../../../context/Admin/User";
+import Input from "../../../components/Input";
 
 // Creator : Team e - chanan
 function Login() {
 
-  const {user, setUser} = useContext(userContext)
+  const { user, setUser } = useContext(userContext)
 
   const Navigation = useNavigate()
-  const users =[
+  const users = [
     {
       firstName: "moshe",
       lastName: "levi",
       email: "teacher@gmail.com",
-      password:"teacher",
+      password: "teacher",
       permissions: "teacher",
     },
     {
       firstName: "yosi",
       lastName: "sade",
       email: "admin@gmail.com",
-      password:"admin",
+      password: "admin",
       permissions: "admin",
     }
   ]
 
   const validation = (email, password) => {
-    let userTecher = users.find((v)=>(email===(v.email) && password===(v.password) && "teacher"===(v.permissions)))
-    let userAdmin = users.find((v)=>(email===(v.email) && password===(v.password) && "admin"===(v.permissions)))
+    let userTecher = users.find((v) => (email === (v.email) && password === (v.password) && "teacher" === (v.permissions)))
+    let userAdmin = users.find((v) => (email === (v.email) && password === (v.password) && "admin" === (v.permissions)))
     // let userCheck = users.find((v)=>(email===(v.email) && password===(v.password)))
-    if (userTecher){
+    if (userTecher) {
       Navigation("/classrooms")
       setUser(userTecher);
     }
-    else if (userAdmin){
+    else if (userAdmin) {
       Navigation("/admin")
       setUser(userAdmin);
     }
@@ -43,46 +44,46 @@ function Login() {
       alert("no permission");
     }
 
-    
+
     safeLocalStoreg(user)
   }
 
   const onSubmit = (e) => {
-      e.preventDefault()
-      const email = e.target.email.value
-      const password = e.target.password.value
-      validation(email, password)
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+    validation(email, password)
   }
 
-  const safeLocalStoreg = (user) =>{
-    console.log(user)}
-      
+  const safeLocalStoreg = (user) => {
+    console.log(user)
+  }
+
 
   return (
     <>
       <div className="all-login">
         <div className="image">
-          <img className="image 1" src={require("./image 1.png")} alt="image 1"/>
-          <img className="image 2" src={require("./image 2.png")} alt="image 2"/>
+          <img className="image 1" src={require("./image 1.png")} alt="image 1" />
+          <img className="image 2" src={require("./image 2.png")} alt="image 2" />
         </div>
         <h1>Welcome back!</h1>
+      </div>
+      <div className="login-box">
+        <div className="login-outbox"></div>
+        <div className="login-inbox">
+          <form onSubmit={onSubmit}>
+            <Input name={"email"} type={"text"} legend={"Email"} valid={"yes"} />
+            <Input name={"password"} type={"password"} legend={"Password"} valid={"yes"} />
+            <Link className="forgot-password" to="/password">{" "}
+              <br />
+              Forgot Password?
+            </Link>
+            <MainButton>Sing in</MainButton>
+          </form>
         </div>
-        <div className="login-box">
-          <div className="login-outbox"></div>
-          <div className="login-inbox">
-            <form onSubmit={onSubmit}>
-              <label name="email">Email id</label>
-              <input type="text" name="email" /> <br />
-              <input type="password" name="password" placeholder="Password" />
-              <Link className="forgot-password" to="/password">{" "}
-                <br />
-                Forgot Password?
-              </Link>
-              <MainButton>Sine in</MainButton>
-            </form>
-          </div>
-        </div>
-      
+      </div>
+
     </>
   );
 }
